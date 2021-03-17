40fadb635c2e98f41149117f46e44314fdf88555
SamuelMumford <60023499+SamuelMumford@users.noreply.github.com>
2021-03-17 13:53:25 -0700

    Add files via upload

--- face_recog/FRTest.py
+++ face_recog/FRTest.py
@@ -23,14 +23,17 @@ from skimage.transform import resize
 from keras.preprocessing.image import img_to_array
 import tensorflow.keras.backend as K
 
-
+#Read in paths for files
 Basepath = sys.argv[1]
 MaskPath = os.path.join(Basepath, 'fake_faces')
 FacePath = os.path.join(Basepath, 'real')
+#Decide if looking for distances to the same face or different faces after demasking
 compSame = False
 
+#Load model
 model = InceptionV3(include_top=False, pooling='avg', input_shape=(256, 256, 3))
 
+#Read in picture files. Only care about order if looking for same face/mask base faces
 _, _, Mfiles = next(os.walk(MaskPath))
 print('final number of mask pictures: ' + str(len(Mfiles)))
 _, _, Ffiles = next(os.walk(FacePath))
@@ -39,6 +42,7 @@ if(compSame):
     Ffiles = sorted(Ffiles)
 print('final number of face pictures: ' + str(len(Ffiles)))
 
+#Make batches of pictures
 def makeBatch(path, files, start, size):
     AllImg = np.zeros((size, 256, 256, 3))
     for i in range(size):
@@ -51,8 +55,10 @@ batch = 100
 imgs = len(Mfiles)
 iters = imgs//batch
 
+#Initialize the array of all encodings
 eMaskAll = np.zeros((0, 2048))
 eFaceAll = np.zeros((0, 2048))
+#Get all the encodings of faces with and without masks
 print('Getting Base Features')
 for i in range(iters):
     print(i/iters)
@@ -62,6 +68,7 @@ for i in range(iters):
     encF = model.predict(fB)
     eMaskAll = np.vstack((eMaskAll, encM))
     eFaceAll = np.vstack((eFaceAll, encF))
+#Find the ``mask" feature
 eMaskMean = np.sum(eMaskAll, axis = 0, keepdims = True)/imgs
 eFaceMean = np.sum(eFaceAll, axis = 0, keepdims = True)/imgs
 print(eMaskMean.shape)
@@ -73,12 +80,14 @@ print(length)
 maskVec = eMaskMean - eFaceMean
 maskVec = maskVec/(np.sqrt(np.dot(maskVec, maskVec.T)[0][0]))
 
+#Get FID distances between face and masked encodings
 print('Get Base FID Distance')
 baseDist = np.mean(np.sum(np.abs(eMaskAll - eFaceAll), axis = 1))
 baseDev = np.std(np.sum(np.abs(eMaskAll - eFaceAll), axis = 1))
 print(baseDist)
 print(baseDev)
 
+#Get stripped face and mask encoding distances
 print('Get Mask Stripped Distance')
 newMask = eMaskAll - np.dot(np.dot(eMaskAll, maskVec.T), maskVec)
 newFace = eFaceAll - np.dot(np.dot(eFaceAll, maskVec.T), maskVec)
@@ -87,10 +96,11 @@ newDev = np.std(np.sum(np.abs(newMask - newFace), axis = 1))
 print(newDist)
 print(newDev)
 
+#Make cosine similarity distance
 def CDist(A, B):
     m = np.mean(np.sum(A*B, axis = 1)/np.sqrt(np.sum(A*A, axis = 1)*np.sum(B*B, axis = 1)))
     d = np.std(np.sum(A*B, axis = 1)/np.sqrt(np.sum(A*A, axis = 1)*np.sum(B*B, axis = 1)))
     return m, d
-
+#Get distances in cosine similarity
 print(CDist(eMaskAll, eFaceAll))
 print(CDist(newMask, newFace))
